cbf1ee646f39b65539c7b0aecc72be7f94486c88
s.yanykin <s.yanykin@corp.mail.ru>
2020-03-01 22:35:44 +0300

    changed init migration

--- migrations/0001_init.sql
+++ migrations/0001_init.sql
@@ -18,17 +18,20 @@ create table messages (
 
 INSERT INTO public.messages ( name, text) VALUES ('start', 'Привет, я делаю гиф из видео!Продолжительность видео должно быть не более 10 сек');
 INSERT INTO public.messages ( name, text) VALUES ('Очистить время начала и конца', 'Время сбросилось, введите новое время');
-INSERT INTO public.messages ( name, text) VALUES ('Новая Gif', 'Отправьте любое видео, выберите время и я сделаю для вас gif');
 INSERT INTO public.messages ( name, text) VALUES ('download error', 'Не получилось загрузить видео, попробуйте позднее');
 INSERT INTO public.messages ( name, text) VALUES ('save video', 'Пожалуйста подождите, сохраняю видео...');
 INSERT INTO public.messages ( name, text) VALUES ('successful download', 'Видео успешно загружено, укажите с какой секунды начать делать gif');
 INSERT INTO public.messages ( name, text) VALUES ('invalid message', 'Извините, я не понимаю.
   				  Если вы хотите сделать gif, выберите нужный пункт из меню');
 INSERT INTO public.messages ( name, text) VALUES ('end second', 'Теперь введите секунду окончания видео');
-INSERT INTO public.messages ( name, text) VALUES ('start create video', 'Обработка видео завершена...\nНачалось создание gif...
-');
-INSERT INTO public.messages ( name, text) VALUES ('loading gif', 'Создание gif завершено\nЗагружаем gif в чат...
-');
 INSERT INTO public.messages ( name, text) VALUES ('end more start', 'Конец не должен быть меньше начала');
 INSERT INTO public.messages ( name, text) VALUES ('video more 10s', 'Продолжительность gif не должно превышать 10 сек');
 INSERT INTO public.messages ( name, text) VALUES ('create video', 'Начало и конец получены, начинаем обработку...');
+INSERT INTO public.messages ( name, text) VALUES ('start create video', 'Обработка видео завершена...
+Началось создание gif...');
+INSERT INTO public.messages ( name, text) VALUES ('loading gif', 'Создание gif завершено
+Загружаем gif в чат...
+
+');
+INSERT INTO public.messages ( name, text) VALUES ('Gif из нового видео', 'Отправьте любое видео, выберите время и я сделаю для вас gif');
+INSERT INTO public.messages ( name, text) VALUES ('Gif из того же видео', 'Введите новое время ');
